e045f481035f0f10030f21dbfb6cd33ef2b6c078
juleskay-b <juliabush00@gmail.com>
2025-01-25 15:27:02 -0500

    new things!

--- main.cpp
+++ main.cpp
@@ -4,8 +4,66 @@
 #include "player.h"
 #include "authentification.h"
 
+using namespace std;
+
+void setText(sf::Text &text, float x, float y) {
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width/2.0f,
+    textRect.top + textRect.height/2.0f);
+    text.setPosition(sf::Vector2f(x, y));
+}
+
+int statsWindow(sf::RenderWindow& window, sf::Font& titleFont, sf::Font& bodyFont, int& width, int& height, player& player) {
+    sf::Text statsTitle;
+    statsTitle.setFont(titleFont);
+    statsTitle.setString("Player!");
+    statsTitle.setStyle(sf::Text::Bold);
+    statsTitle.setCharacterSize(24);
+    setText(statsTitle, (width/2), (height/2)-150);
+
+    window.display();
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.clear();
+                window.close();
+                return 0;
+            }
+        }
+        window.draw(statsTitle);
+        window.display();
+    }
+
+    return 0;
+}
+
 int main() {
+    sf::RenderWindow window;
+    int width = 800;
+    int height = 600;
+
     player newPlayer;
     newPlayer.printTimePlayed();
+
+    sf::Font titleFont;
+    titleFont.loadFromFile("files/happySchool.ttf");
+
+    sf::Font bodyFont;
+    bodyFont.loadFromFile("files/BebasNeue-Regular.ttf");
+
+    statsWindow(window, titleFont, bodyFont, width, height, newPlayer);
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
