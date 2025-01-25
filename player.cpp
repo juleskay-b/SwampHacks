259aad3860654e8e198b27dd5159d74d226769d1
juleskay-b <juliabush00@gmail.com>
2025-01-25 14:44:13 -0500

    adding time functionality (local only)

--- player.cpp
+++ player.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "player.h"
+
+Time::Time() {
+    start = chrono::steady_clock::now();
+}
+
+int Time::getHours() {
+    chrono::steady_clock::time_point thisTime = chrono::steady_clock::now();
+    chrono::duration<int> time_span = chrono::duration_cast<chrono::duration<int>>(thisTime - start);
+    int totalSeconds = time_span.count();
+    int totalHours = totalSeconds / 3600;
+    return totalHours;
+}
+
+
+player::player() {
+
+}
+
+void player::printTimePlayed() {
+    cout << "You have been playing for " << startTime.getHours() << " hours!" << endl;
+}
--- player.h
+++ player.h
@@ -5,10 +5,22 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <chrono>
+#include <iostream>
+using namespace std;
 
+struct Time {
+    chrono::steady_clock::time_point start;
+public:
+    Time();
+    int getHours();
+};
 
 class player {
-
+    Time startTime;
+public:
+    player();
+    void printTimePlayed();
 };
 
 
