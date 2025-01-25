9e70b4e70426328f621b7807213f88f3e800da32
anawsai <anapetrela8@gmail.com>
2025-01-25 18:03:15 -0500

    let's see if this works

---
+++ cmake-build-debug/CMakeFiles/scrap.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/anapetrela/Documents/GitHub/SwampHacks/authentification.cpp" "CMakeFiles/scrap.dir/authentification.cpp.o" "gcc" "CMakeFiles/scrap.dir/authentification.cpp.o.d"
+  "/Users/anapetrela/Documents/GitHub/SwampHacks/main.cpp" "CMakeFiles/scrap.dir/main.cpp.o" "gcc" "CMakeFiles/scrap.dir/main.cpp.o.d"
+  "/Users/anapetrela/Documents/GitHub/SwampHacks/pet.cpp" "CMakeFiles/scrap.dir/pet.cpp.o" "gcc" "CMakeFiles/scrap.dir/pet.cpp.o.d"
+  "/Users/anapetrela/Documents/GitHub/SwampHacks/player.cpp" "CMakeFiles/scrap.dir/player.cpp.o" "gcc" "CMakeFiles/scrap.dir/player.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/scrap.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scrap.dir/authentification.cpp.o"
+  "CMakeFiles/scrap.dir/authentification.cpp.o.d"
+  "CMakeFiles/scrap.dir/main.cpp.o"
+  "CMakeFiles/scrap.dir/main.cpp.o.d"
+  "CMakeFiles/scrap.dir/pet.cpp.o"
+  "CMakeFiles/scrap.dir/pet.cpp.o.d"
+  "CMakeFiles/scrap.dir/player.cpp.o"
+  "CMakeFiles/scrap.dir/player.cpp.o.d"
+  "scrap"
+  "scrap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
