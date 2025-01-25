8105192e2e462ab005475f5276ea990e0d3317da
Hmorel8263 <HectorMorel8263@outlook.com>
2025-01-25 15:45:42 -0500

    I did the Pet Class (h and cpp)

--- pet.h
+++ pet.h
@@ -5,10 +5,30 @@
 #ifndef PET_H
 #define PET_H
 
+using namespace std;
+#include <iostream>
+#include <string>
 
 
-class pet {
+class Pet {
+private:
+    string name;
+    int health = 75;
+    int points;
+public:
 
+    //Constructor and Parameterized Constructor
+    //Pet();
+    Pet(string name, int health);
+
+    //Other Functions
+    string Status(int health);
+    void Print();
+
+    //Getters
+    string getName();
+    int getHealth();
+    int getStatus();
 };
 
 
