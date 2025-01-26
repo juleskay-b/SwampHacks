d547b5c8cd3dcec5bc5f4d143968f883fc73e269
juleskay-b <juliabush00@gmail.com>
2025-01-26 09:40:42 -0500

    Ok final

---
+++ cmake-build-release/CMakeFiles/scrap.dir/authentification.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/scrap.dir/authentification.cpp.obj: \
+ C:\Users\Jules\CLionProjects\scrap\authentification.cpp \
+ C:\Users\Jules\CLionProjects\scrap\authentification.h
---
+++ cmake-build-release/CMakeFiles/scrap.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scrap.dir/Math.cpp.obj"
+  "CMakeFiles/scrap.dir/Math.cpp.obj.d"
+  "CMakeFiles/scrap.dir/Sprites.cpp.obj"
+  "CMakeFiles/scrap.dir/Sprites.cpp.obj.d"
+  "CMakeFiles/scrap.dir/authentification.cpp.obj"
+  "CMakeFiles/scrap.dir/authentification.cpp.obj.d"
+  "CMakeFiles/scrap.dir/main.cpp.obj"
+  "CMakeFiles/scrap.dir/main.cpp.obj.d"
+  "CMakeFiles/scrap.dir/pet.cpp.obj"
+  "CMakeFiles/scrap.dir/pet.cpp.obj.d"
+  "CMakeFiles/scrap.dir/player.cpp.obj"
+  "CMakeFiles/scrap.dir/player.cpp.obj.d"
+  "libscrap.dll.a"
+  "scrap.exe"
+  "scrap.exe.manifest"
+  "scrap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
